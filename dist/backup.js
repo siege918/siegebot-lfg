2619fa935fb7af8565d3de33b6f031e6c3063037
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-09-20 10:39:37 -0700

    updated the built files since we don't need declarations

--- dist/backup.js
+++ dist/backup.js
@@ -1,87 +1,98 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Constants = require("./constants");
-const aws_sdk_1 = require("aws-sdk");
-const accessKeyID = process.env[Constants.S3_ACCESS_KEY_ENV] || "";
-const secretAccessKey = process.env[Constants.S3_SECRET_ENV] || "";
-const bucketName = process.env[Constants.S3_BUCKET_ENV] || "";
-const region = process.env[Constants.S3_REGION_ENV] || "us-west-2";
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const aws_sdk_1 = require('aws-sdk');
+const Constants = require('./constants');
+const accessKeyID = process.env[Constants.S3_ACCESS_KEY_ENV] || '';
+const secretAccessKey = process.env[Constants.S3_SECRET_ENV] || '';
+const bucketName = process.env[Constants.S3_BUCKET_ENV] || '';
+const region = process.env[Constants.S3_REGION_ENV] || 'us-west-2';
 let S3Client;
 if (accessKeyID && secretAccessKey && bucketName) {
-    S3Client = new aws_sdk_1.S3({ region, credentials: new aws_sdk_1.Credentials(accessKeyID, secretAccessKey) });
+  S3Client = new aws_sdk_1.S3({
+    region,
+    credentials: new aws_sdk_1.Credentials(accessKeyID, secretAccessKey)
+  });
 }
 function restore(groupCache) {
-    if (!S3Client)
-        return;
-    S3Client.getObject({
-        Bucket: bucketName,
-        Key: 'latest-lfg-backup',
-    }, (err, data) => {
-        if (err) {
-            console.warn(err);
+  if (!S3Client) return;
+  S3Client.getObject(
+    {
+      Bucket: bucketName,
+      Key: 'latest-lfg-backup'
+    },
+    (err, data) => {
+      if (err) {
+        console.warn(err);
+      } else {
+        let body = data.Body || '';
+        var latestBackupName = body.toString().trim();
+        if (latestBackupName) {
+          restoreBackupByName(groupCache, latestBackupName);
         }
-        else {
-            let body = data.Body || '';
-            var latestBackupName = body.toString().trim();
-            if (latestBackupName) {
-                restoreBackupByName(groupCache, latestBackupName);
-            }
-        }
-    });
+      }
+    }
+  );
 }
 exports.restore = restore;
 function restoreBackupByName(groupCache, latestBackupName) {
-    S3Client.getObject({
-        Bucket: bucketName,
-        Key: latestBackupName
-    }, (err, data) => {
-        if (err) {
-            console.warn(err);
+  S3Client.getObject(
+    {
+      Bucket: bucketName,
+      Key: latestBackupName
+    },
+    (err, data) => {
+      if (err) {
+        console.warn(err);
+      } else {
+        let body = data.Body || '';
+        var latestBackup = body.toString().trim();
+        if (latestBackup) {
+          groupCache.import(latestBackup);
         }
-        else {
-            let body = data.Body || '';
-            var latestBackup = body.toString().trim();
-            if (latestBackup) {
-                groupCache.import(latestBackup);
-            }
-        }
-    });
+      }
+    }
+  );
 }
 function backup(groupCache) {
-    if (!S3Client)
-        return;
-    let backupDate = new Date();
-    let backupName = `LFG_${backupDate.toISOString()}.json`;
-    console.log(groupCache);
-    console.log(groupCache.export());
-    S3Client.upload({
-        Bucket: bucketName,
-        Key: backupName,
-        Body: groupCache.export()
-    }, (err, data) => {
-        if (err) {
-            console.warn('Error marking backup file as latest.');
-            console.warn(err);
-        }
-        else {
-            console.log(`File ${backupName} has been uploaded to ${bucketName}`);
-            markFileAsLatest(backupName);
-        }
-    });
+  if (!S3Client) return;
+  let backupDate = new Date();
+  let backupName = `LFG_${backupDate.toISOString()}.json`;
+  console.log(groupCache);
+  console.log(groupCache.export());
+  S3Client.upload(
+    {
+      Bucket: bucketName,
+      Key: backupName,
+      Body: groupCache.export()
+    },
+    (err, data) => {
+      if (err) {
+        console.warn('Error marking backup file as latest.');
+        console.warn(err);
+      } else {
+        console.log(`File ${backupName} has been uploaded to ${bucketName}`);
+        markFileAsLatest(backupName);
+      }
+    }
+  );
 }
 exports.backup = backup;
 function markFileAsLatest(fileName) {
-    S3Client.upload({
-        Bucket: bucketName,
-        Key: 'latest-lfg-backup',
-        Body: fileName
-    }, (err, data) => {
-        if (err) {
-            console.warn('Error marking backup file as latest.');
-            console.warn(err);
-        }
-        else {
-            console.log(`File ${fileName} has been marked as latest in bucket ${bucketName}`);
-        }
-    });
+  S3Client.upload(
+    {
+      Bucket: bucketName,
+      Key: 'latest-lfg-backup',
+      Body: fileName
+    },
+    (err, data) => {
+      if (err) {
+        console.warn('Error marking backup file as latest.');
+        console.warn(err);
+      } else {
+        console.log(
+          `File ${fileName} has been marked as latest in bucket ${bucketName}`
+        );
+      }
+    }
+  );
 }
--- dist/groupCache.js
+++ dist/groupCache.js
@@ -1,110 +1,130 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const group_1 = require("./group");
-const moment = require("moment");
-const shortid_1 = require("shortid");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const moment = require('moment');
+const shortid_1 = require('shortid');
+const group_1 = require('./group');
 class GroupCache {
-    constructor() {
-        this._cache = new Map();
+  constructor() {
+    this.cache = new Map();
+  }
+  has(id) {
+    return this.cache.has(id);
+  }
+  get(id) {
+    const group = this.cache.get(id);
+    if (!group) {
+      throw new RangeError('The specified group does not exist.');
     }
-    has(id) {
-        return this._cache.has(id);
+    return group;
+  }
+  print(id, doMention = false) {
+    const group = this.cache.get(id);
+    if (!group) {
+      return '';
     }
-    get(id) {
-        let group = this._cache.get(id);
-        if (!group) {
-            throw new RangeError('The specified group does not exist.');
-        }
-        return group;
-    }
-    print(id, doMention = false) {
-        let group = this._cache.get(id);
-        if (!group) {
-            return '';
-        }
-        return (`${group.print(doMention)}
+    return `${group.print(doMention)}
 *Join this group by typing* \`\`\`.join ${id}\`\`\`
 *Leave this group by typing* \`\`\`.leave ${id}\`\`\`
-`);
+`;
+  }
+  printAll() {
+    return [...this.cache.keys()]
+      .map(id => this.print(id))
+      .join('\n---------------------------------\n\n');
+  }
+  get15MinuteGroups() {
+    const groups = [...this.cache.values()].filter(
+      group =>
+        !group.hasHad15MinuteUpdate &&
+        moment()
+          .add(15, 'minutes')
+          .isAfter(group.startTime)
+    );
+    groups.forEach(group => {
+      group.hasHad15MinuteUpdate = true;
+    });
+    return groups;
+  }
+  getStartingGroups() {
+    const groups = [...this.cache.values()].filter(
+      group => !group.hasHadStartingUpdate && moment().isAfter(group.startTime)
+    );
+    groups.forEach(group => {
+      group.hasHadStartingUpdate = true;
+    });
+    return groups;
+  }
+  housekeep() {
+    for (const group of this.cache.values()) {
+      if (group.hasHadStartingUpdate) {
+        this.cache.delete(group.id);
+      }
     }
-    printAll() {
-        return [...this._cache.keys()].map((id) => this.print(id)).join('\n---------------------------------\n\n');
+  }
+  create(creatorMember, gameName, maxPlayers, startTime, channel) {
+    let id = '';
+    do {
+      id = shortid_1.generate().substring(0, 4);
+    } while (this.has(id));
+    const creator = {
+      Id: creatorMember.id,
+      Tag: creatorMember.user.tag,
+      Mention: creatorMember.toString()
+    };
+    const players = new Map();
+    players.set(creatorMember.id, creator);
+    const groupData = {
+      id,
+      gameName,
+      players,
+      maxPlayers,
+      startTime: startTime.toDate(),
+      creator,
+      channel
+    };
+    this.cache.set(id, new group_1.Group(groupData));
+    return id;
+  }
+  remove(creator, id) {
+    const group = this.get(id);
+    if (group.creator.Id !== creator) {
+      throw new Error('Groups can only be removed by their creator.');
     }
-    get15MinuteGroups() {
-        let groups = [...this._cache.values()].filter((group) => !group.hasHad15MinuteUpdate && moment().add(15, 'minutes').isAfter(group.startTime));
-        groups.forEach((group) => { group.hasHad15MinuteUpdate = true; });
-        return groups;
-    }
-    getStartingGroups() {
-        let groups = [...this._cache.values()].filter((group) => !group.hasHadStartingUpdate && moment().isAfter(group.startTime));
-        groups.forEach((group) => { group.hasHadStartingUpdate = true; });
-        return groups;
-    }
-    housekeep() {
-        for (let group of this._cache.values()) {
-            if (group.hasHadStartingUpdate) {
-                this._cache.delete(group.id);
-            }
+    this.cache.delete(id);
+    return group;
+  }
+  joinGroup(player, id) {
+    const group = this.get(id);
+    group.addPlayer(player);
+    return group;
+  }
+  leaveGroup(player, id) {
+    const group = this.get(id);
+    group.removePlayer(player);
+    return group;
+  }
+  export() {
+    return JSON.stringify(this.cache);
+  }
+  import(data) {
+    const importData = new Map(JSON.parse(data));
+    const newCache = new Map();
+    for (const key of importData.keys()) {
+      const importDatum = importData.get(key);
+      if (importDatum) {
+        if (
+          importDatum.players &&
+          typeof importDatum.players[Symbol.iterator] === 'function'
+        ) {
+          importDatum.players = new Map(importDatum.players);
+        } else {
+          importDatum.players = new Map();
         }
+        newCache.set(key, new group_1.Group(importDatum));
+      }
     }
-    create(creatorMember, gameName, maxPlayers, startTime, channel) {
-        let id = '';
-        do {
-            id = shortid_1.generate().substring(0, 4);
-        } while (this.has(id));
-        let creator = { Id: creatorMember.id, Tag: creatorMember.user.tag, Mention: creatorMember.toString() };
-        let players = new Map();
-        players.set(creatorMember.id, creator);
-        let groupData = {
-            id,
-            gameName,
-            players,
-            maxPlayers,
-            startTime: startTime.toDate(),
-            creator,
-            channel
-        };
-        this._cache.set(id, new group_1.Group(groupData));
-        return id;
-    }
-    remove(creator, id) {
-        let group = this.get(id);
-        if (group.creator.Id !== creator) {
-            throw new Error('Groups can only be removed by their creator.');
-        }
-        this._cache.delete(id);
-        return group;
-    }
-    joinGroup(player, id) {
-        let group = this.get(id);
-        group.addPlayer(player);
-        return group;
-    }
-    leaveGroup(player, id) {
-        let group = this.get(id);
-        group.removePlayer(player);
-        return group;
-    }
-    export() {
-        return JSON.stringify(this._cache);
-    }
-    import(data) {
-        let importData = new Map(JSON.parse(data));
-        let newCache = new Map();
-        for (let key of importData.keys()) {
-            let importDatum = importData.get(key);
-            if (importDatum) {
-                if (importDatum.players && typeof importDatum.players[Symbol.iterator] === 'function') {
-                    importDatum.players = new Map(importDatum.players);
-                }
-                else {
-                    importDatum.players = new Map();
-                }
-                newCache.set(key, new group_1.Group(importDatum));
-            }
-        }
-        this._cache = newCache;
-        return this._cache;
-    }
+    this.cache = newCache;
+    return this.cache;
+  }
 }
 exports.GroupCache = GroupCache;
