791e2e7c3a50ea3b94d2b91631500c6fd6a3d6f2
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-10-16 11:01:25 -0700

    added a test to revive the cache properly

--- lib/GroupCache.spec.ts
+++ lib/GroupCache.spec.ts
@@ -3,6 +3,7 @@ import test from 'ava';
 import { Client, Guild, GuildMember } from 'discord.js';
 import * as moment from 'moment';
 
+import Group from './Group';
 import GroupCache from './GroupCache';
 
 test('exports the cache properly', t => {
@@ -17,9 +18,48 @@ test('exports the cache properly', t => {
   const parsed = JSON.parse(cache.export());
 
   t.is(parsed.jsontype, 'JSMap');
+  t.deepEqual(parsed.metadata, { key: 'string', value: 'Group' });
   t.assert(parsed.jsondata instanceof Array);
   t.is(parsed.jsondata.length, 1);
   t.assert(parsed.jsondata[0] instanceof Array); // is a tuple
   t.assert(typeof parsed.jsondata[0][0] === 'string'); // has a key
   t.assert(typeof parsed.jsondata[0][1] === 'object'); // has a value
+});
+
+test('imports the cache properly', t => {
+  const jsonifiedCache = {
+    jsontype: 'JSMap',
+    metadata: { key: 'string', value: 'Group' },
+    jsondata: [
+      [
+        'EWhj',
+        {
+          id: 'EWhj',
+          gameName: 'test game',
+          players: {
+            jsontype: 'JSMap',
+            metadata: { key: 'string', value: 'object' },
+            jsondata: [
+              [
+                'test_user_id',
+                { Id: 'test_user_id', Mention: '<@test_user_id>' }
+              ]
+            ]
+          },
+          maxPlayers: 6,
+          startTime: '2019-10-16T17:57:10.022Z',
+          startTimeMoment: '2019-10-16T17:57:10.022Z',
+          creator: { Id: 'test_user_id', Mention: '<@test_user_id>' },
+          channel: 'test channel',
+          hasHad15MinuteUpdate: false,
+          hasHadStartingUpdate: false
+        }
+      ]
+    ]
+  };
+
+  const cache = new GroupCache();
+  cache.import(JSON.stringify(jsonifiedCache));
+
+  t.assert(cache.get('EWhj') instanceof Group);
 });
